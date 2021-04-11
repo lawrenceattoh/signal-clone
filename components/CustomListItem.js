bffb9727292a66ae0d1e9f58f7801ce75ca96160
Lawrence <lawrenceattoh@ymail.com>
2021-04-11 21:32:10 +0100

    Final Commit

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Avatar } from 'react-native-elements'
+import { db } from '../firebase'
 
 const CustomListItem = ({ id, chatName, enterChat }) => {
+    const [chatMessages, setChatMessages] = useState([])
+
+    useEffect(() => {
+        const unsubscribe = db
+            .collection('chats')
+            .doc(id)
+            .collection('messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot((snapshot) =>
+                setChatMessages(snapshot.docs.map((doc) => doc.data()))
+            );
+
+        return unsubscribe;
+    })
+
     return (
-        <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
+        <ListItem
+            key={id}
+            onPress={() => enterChat(id, chatName)}
+            key={id}
+            bottomDivider
+        >
             <Avatar
                 rounded
                 source={{
-                    uri:
-                        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png"
+                    uri: chatMessages?.[0]?.photoURL ||
+                        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
                 }}
             />
             <ListItem.Content>
@@ -17,8 +38,8 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
                     {chatName}
                 </ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
-                    ABCD
-              </ListItem.Subtitle>
+                    {chatMessages?.[0]?.displayName}: {chatMessages?.[0]?.message}
+                </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
     )
